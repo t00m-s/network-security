68e4b070d866aa555e2d14abce5dbf9f56e9baff
Alvise Favero <66018479+alvi-se@users.noreply.github.com>
2025-03-12 22:20:18 +0100

    Bug fix server

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-serverclient:
+server:
 	gcc -Wall -Wextra -pedantic traceroute_server.c -o traceroute_server
 sender:
 	gcc -Wall -Wextra -pedantic traceroute_sender.c -o traceroute_sender
--- traceroute_server.c
+++ traceroute_server.c
@@ -12,6 +12,7 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include "common.h"
 
 // NOTE: in the slides there are ipheader and udpheader,
 // they are identical to the linux ones so...
@@ -21,16 +22,15 @@ typedef struct ttl_exceeded_header {
   u_int8_t code;
   u_int16_t checksum;
   u_int32_t unused;
-  struct iphdr internet_header;
-  u_int64_t original_datagram_first_64_bits;
-} ttl_exceeded_header;
+} ttl_exceeded_header_t;
 
 void filter_icmp(char *buffer);
 
-int main(void) {
+int main(int argc, char* argv[]) {
   struct packet_mreq mr;
   size_t buf_size = 1500;
   char buf[buf_size];
+  char* interface;
   // 1) Open a raw socket
   // Opens raw socket.
   // AF_PACKET means low-level packet interface
@@ -38,9 +38,26 @@ int main(void) {
   // ETH_P_ALL means, just read everything.
   int raw_socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
   if (raw_socket == -1) {
-    perror("Usage: sudo ./traceroute_server");
+    perror("Usage: sudo ./traceroute_server <interface_name>\n");
     exit(EXIT_FAILURE);
   }
+  
+  if (argc < 2) {
+    perror("Usage: sudo ./traceroute_server <interface_name>\n");
+    close(raw_socket);
+    exit(EXIT_FAILURE);
+  }
+  interface = argv[1];
+
+  // Get interface index
+  mr.mr_ifindex = if_nametoindex(interface);
+  if (mr.mr_ifindex == 0) {
+    perror("if_nametoindex failed");
+    close(raw_socket);
+    exit(EXIT_FAILURE);
+  }
+
+
   // 2) Put the NIC in promiscuous mode
   // Adding promiscuous mode to the socket
   mr.mr_type = PACKET_MR_PROMISC;
@@ -55,6 +72,7 @@ int main(void) {
     // Technically we're intercepting all packets here.
     bzero(buf, buf_size);
     ssize_t data_size = recvfrom(raw_socket, buf, buf_size, 0, NULL, NULL);
+    // printf("Received %ld bytes of data\n", data_size);
     // Either error or 0 read bytes.
     if (data_size <= 0) {
       close(raw_socket);
@@ -73,16 +91,16 @@ void filter_icmp(char *buffer) {
   // Filter ICMP packets
   // cat /etc/protocols says that icmp has protocol == 1
   // I trust it.
-  struct iphdr *ip_1_header =
-      (struct iphdr *)(buffer + sizeof(struct ether_header));
-  if (ip_1_header->protocol != 1)
+  struct ipheader *ip_1_header =
+      (struct ipheader *)(buffer + sizeof(struct ether_header));
+  if (ip_1_header->iph_protocol != 1)
     return;
 
   // Jumping to the ICMP header
   // ihl is the length of the IP header in 4-bytes
   struct ttl_exceeded_header *icmp_h =
       (struct ttl_exceeded_header *)(buffer + sizeof(struct ether_header) +
-                                     (ip_1_header->ihl * 4));
+                                     (ip_1_header->iph_ihl * 4));
   // Filtering
   if (icmp_h->type != 11)
     return;
@@ -92,20 +110,20 @@ void filter_icmp(char *buffer) {
   // From the ICMP packet, you need to read the source IP (not the one in the
   // payload)
   struct in_addr icmp_saddr;
-  icmp_saddr.s_addr = ip_1_header->saddr;
+  icmp_saddr.s_addr = ip_1_header->iph_sourceip.s_addr;
 
   // From the packet included payload, you need to skip the IP and UDP header,
   // and read the payload
   // Skipping all the headers
   size_t total_skip_size = sizeof(struct ether_header) +
-                           (ip_1_header->ihl * 4) +
+                           sizeof(struct ipheader) +
                            sizeof(struct ttl_exceeded_header);
   // Now we're in payload.
   void *payload = (void *)(buffer + total_skip_size);
   // Skipping ip header of payload
-  size_t ip_2_header_size = (((struct iphdr *)(payload))->ihl * 4);
+  size_t ip_2_header_size = (((struct ipheader *)(payload))->iph_ihl * 4);
   total_skip_size += ip_2_header_size;
-  total_skip_size += sizeof(struct udphdr);
+  total_skip_size += sizeof(struct udpheader);
 
   u_int8_t *ttl = (u_int8_t *)(buffer + total_skip_size);
   printf("Source IP: %s \t TTL: %u\n", inet_ntoa(icmp_saddr), *ttl);
